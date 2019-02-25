e5f0133aa4987db9e91c766f7a9099415bba190d
abdultolba <abdultolba@wayne.edu>
2019-02-24 19:19:37 -0500

    Implemented clicking functionality.

--- Day 01: JavaScript Drum Kit/js/source.js
+++ Day 01: JavaScript Drum Kit/js/source.js
@@ -1,13 +1,24 @@
 function playSound(event){
     // Retrieve the html audio element based on the key pressed
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 
     if(!audio) return;              // If the element doesn't exist (keycode not in our data-key set), exit the function.
     audio.currentTime = 0;          // Rewind to the beginning to allow the keys to play repeatedly without lag.
     audio.play();
 
     key.classList.add('playing');   // Add the class playing to the key to get it an animated effect 
+}
+
+function clickTrigger(event){
+    const key = this.dataset.key;   // Retrieve the key (data-key="_") using this.dataset.key
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+    const el = document.querySelector(`.key[data-key="${key}"]`);
+
+    audio.currentTime = 0;
+    audio.play();
+
+    el.classList.add('playing');
 }
 
 function removeTransition(event){
@@ -17,7 +28,9 @@ function removeTransition(event){
 
 // Retrieve all the elements that transitioned and remove the effects when the transition ends
 const keys = document.querySelectorAll(".key"); 
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => key.addEventListener('mousedown', clickTrigger));
 
 // This event listener will play the corresponding audio file when a key is clicked and will animate it.
 window.addEventListener("keydown", playSound);
+
